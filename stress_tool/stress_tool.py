af22d371e0933a04e87ca5451face1490d670874
Peter Razumovsky <prazumovsky@mirantis.com>
2017-11-02 13:40:26 +0400

    Add setup.py and few more funny things

--- stress_tool/stress_tool.py
+++ stress_tool/stress_tool.py
@@ -1,14 +1,22 @@
 import argparse
 import random
+import signal
+import sys
 import time
 
 import prometheus_client as pclient
+
+# strange thing
+def signal_handler(signal, frame):
+        print('Target stopped.')
+        sys.exit(0)
+signal.signal(signal.SIGINT, signal_handler)
 
 pc = pclient.ProcessCollector(namespace='docker',
                               pid=lambda: open('/var/run/docker.pid').read())
 
 
-def load_memory(a, s):
+def load_memory(s):
     pc.collect()
     flag = True
     for ss in s:
@@ -16,20 +24,19 @@ def load_memory(a, s):
             ss.set(random.randint(10**6, 10**7))
             flag = not flag
         else:
-            ss.observe(random.randint(10**6, 10**7))
+            ss.observe(random.randint(0, 100) / 100)
             flag = not flag
-    return a
 
 
 def parse_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--listen-address', default=8080)
-    parser.add_argument('--metric-count', default=3)
-    parser.add_argument('--message-frequency', default=15000)
+    parser.add_argument('-P', '--listen-address', default=8080)
+    parser.add_argument('-n', '--metric-count', default=3)
+    parser.add_argument('-s', '--message-frequency', default=15000)
     return parser
 
 
-if __name__ == '__main__':
+def main():
     parser = parse_args().parse_args()
     # Start up the server to expose the metrics.
     pclient.start_http_server(int(parser.listen_address))
@@ -40,6 +47,9 @@ if __name__ == '__main__':
         s.append(pclient.Histogram('random_metric_histogram_%s' % i,
                                    'Random metric histogram'))
     while True:
-        a = []
-        a = load_memory(a, s)
+        load_memory(s)
         time.sleep(float(parser.message_frequency) * 0.001)
+
+
+if __name__ == '__main__':
+    main()
