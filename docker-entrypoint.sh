68037c987422814173a85955099b538c8c0f8b42
Peter Razumovsky <prazumovsky@mirantis.com>
2017-11-28 15:53:15 +0400

    Fix docker entrypoint. Now all works fine.

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,5 +1,8 @@
-#!/bin/bash
-if [[ $READ ]]; then
+#!/usr/bin/env bash
+
+set -eux
+
+if [ $READ = true ]; then
     prom-stress-tool -r -l $PROMETHEUS -rp $READ_PERIOD -j $JOB_NAME -i $INSTANCE;
 else
     prom-stress-tool -P $PORT -n $COUNT -s $SPEED;
